8b6b6e46edac9cbc5eb45efaad98aa32b354d3e0
FilipeAl78 <filipe.floriano.santos@escola.pr.gov.br>
2024-10-02 11:20:02 -0300

    Add files via upload

---
+++ Alura-project-main/FIlipe-ALuraproject/perguntas.js
@@ -0,0 +1,35 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
+
+criaCartao(
+    'Lingua portuguesa',
+    'O que é um verbo?',
+    'Uma ação.'
+)
+
+criaCartao(
+    'Matemática',
+    'Qual a soma de todos os números entre 1 e 100?',
+    '5050'
+)
